143f62b1e1042d133ac7846757e0f024f5fdce0b
XuanKong <k.xuan29@gmail.com>
2019-08-04 14:43:59 -0700

    add Product Review Route

--- src/container/Main.js
+++ src/container/Main.js
@@ -4,33 +4,39 @@ import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import StockDetailContainer from "./StockDetailContainer";
 import HomeContainer from "./HomeContainer"
 import NavigationComponent from "../component/NavigationComponent";
-
+import ProductReviewContainer from "./ProductReviewContainer";
 class Main extends Component {
     render() {
         return (
             <div>
-            <Router>
-                <NavigationComponent/>
-                <div>
-                    <Route
-                        path = '/home'
-                        component = {HomeContainer} />
-                </div>
-                <div>
-                    <Route
-                        path='/search'
-                        component={SearchStockContainer}
-                    />
-                </div>
+                <Router>
+                    <NavigationComponent/>
+                    <div>
+                        <Route
+                            path='/home'
+                            component={HomeContainer}/>
+                    </div>
+                    <div>
+                        <Route
+                            path='/search'
+                            component={SearchStockContainer}
+                        />
+                    </div>
 
-                <div>
-                    <Route
-                        path='/detail/:urlKey'
-                        component={StockDetailContainer}
+                    <div>
+                        <Route
+                            path='/detail/:urlKey'
+                            component={StockDetailContainer}
+                        />
+                    </div>
+                    <div>
+                        <Route
+                            path='/detail/:urlKey'
+                            component={ProductReviewContainer}
+                        />
+                    </div>
 
-                    />
-                </div>
-            </Router>
+                </Router>
             </div>
         );
     }
