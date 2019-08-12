fef3bf73ded3e63250f6f47916e1fa2d3d77c3c8
bleachalon <qin.ha@husky.neu.edu>
2019-08-11 23:43:02 -0800

    Update NavigationComponent.js

--- src/component/NavigationComponent.js
+++ src/component/NavigationComponent.js
@@ -104,7 +104,7 @@ export default function NavigationComponent() {
                 {Cookies.get("JSESSIONID") ==null &&<Link to='/signup'><Button>Sign Up</Button></Link>}
                 {Cookies.get("JSESSIONID") !=null && <Link to="/home"><Button  onClick={()=>logout()} >Sign Out</Button></Link>}
                     <Link to='/search'><Button>Search</Button></Link>
-                    <Link to='/user/1/profile'><Button >My Account</Button></Link>
+                {Cookies.get("JSESSIONID") ==null &&<Link to='/user/1/profile'><Button >My Account</Button></Link>}
             </Toolbar>
             </AppBar>
         </div>
