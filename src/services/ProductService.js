4e8c6f56ac80e35e443b3e6012b86cf1175cc580
bleachalon <qin.ha@husky.neu.edu>
2019-08-11 16:08:34 -0800

    post product unfinish

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -10,6 +10,13 @@ export default class ProductService{
         return this.myInstance;
     }
 
+    addProduct = (urlKey, uid)=>
+        fetch(this.url+ "product/" + urlKey + "/" +uid, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            }}).then(response => response.json())
+
     findAllProduct =() =>
         fetch(this.url +'product').then(response => response.json())
 }
