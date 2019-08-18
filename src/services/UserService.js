8be41739150e0d11c0d9fb626da087d1fcfbad64
XuanKong <k.xuan29@gmail.com>
2019-08-18 02:36:45 -0700

    h

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,5 +1,6 @@
 export default class UserService {
     url = "http://www.webdev5610.site/api/";
+    url2 = "http://localhost:8080/api/";
     urlHeroku = 'https://peaceful-savannah-55840.herokuapp.com/api/'
     static myInstance = null;
 
@@ -23,7 +24,7 @@ export default class UserService {
         }).then(res => res)
 
     login = (user) =>
-        fetch(this.url + "login", {
+        fetch(this.url2 + "login", {
             method: "POST",
             body: JSON.stringify(user),
             headers: {
